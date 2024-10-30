2d14c1e2b96573793ec9a2e8b00cf10e3e91dcc0
Dorjsembe.P <58542317+168-artisticsembe-117@users.noreply.github.com>
2024-10-30 16:11:24 +0800

    Add files via upload

---
+++ Java Advanced/1.Stack and Queue/Lab/P01_BrowserHistory.java
@@ -0,0 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<String> browser = new ArrayDeque<>();
+
+        String line = scanner.nextLine();
+
+        String curretn = "";
+
+        while(!line.equals("Home")){
+            if(line.equals("back")){
+                if(!browser.isEmpty()) {
+                    curretn = browser.pop();
+                }else{
+                    System.out.println("no previous URLs");
+                    line = scanner.nextLine();
+                    continue;
+                }
+            }else {
+                if(!curretn.equals("")) {
+                    browser.push(curretn);
+                }
+                curretn = line;
+            }
+            System.out.println(curretn);
+            line = scanner.nextLine();
+        }
+
+    }
+}
